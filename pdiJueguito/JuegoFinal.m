7b5887f80833a25b0a712c0d798f7f1e1d595a70
Nitromejtobon <nitromejtobon@hotmail.es>
2018-09-10 21:43:39 -0500

    entercito

--- pdiJueguito/JuegoFinal.m
+++ pdiJueguito/JuegoFinal.m
@@ -10,8 +10,8 @@ set(vid,'FramesPerTrigger',1);
 set(vid,'TriggerRepeat',Inf);
 start(vid);
 while( vid.FramesAcquired <= 10000)
-snapshot = getsnapshot(vid);%Capturamos la imagen de la cámara
-cdt = flip(snapshot,2);%Aplicamos la función flip, que nos permite rotar la imagen y así evitar el efecto de espejo
+snapshot = getsnapshot(vid);%Capturamos la imagen de la cï¿½mara
+cdt = flip(snapshot,2);%Aplicamos la funciï¿½n flip, que nos permite rotar la imagen y asï¿½ evitar el efecto de espejo
 cdt2 = cdt;[fill,col,cap]=size(cdt2);%Realizamos una copia de la imagen
 
 
@@ -19,3 +19,4 @@ cdt2 = cdt;[fill,col,cap]=size(cdt2);%Realizamos una copia de la imagen
 guitarra=imread('Guitarra.png');guitarra=imresize(guitarra,[fill,col]);
 guitarra = (guitarra*1.5)+(cdt2*0.8); 
 end
+stop(vid);
